cc9aa91294f2661efa3c85d47625dd7c16192439
David Gayerie <dagaydevel@free.fr>
2015-09-18 12:53:33 +0200

    refacto + ajout du code 403 si on tente d'accéder à un fichier en dehors de ~/Public

--- 02_server/http_file_server.sh
+++ 02_server/http_file_server.sh
@@ -13,34 +13,45 @@
 #
 #########################################################
 
+function send_http_error ()
+{
+  local STATUS_CODE=$1
+  local MESSAGE="$2"
+
+  echo "HTTP/1.1 $STATUS_CODE $MESSAGE"
+  echo "Content-type: text/plain"
+  echo "Content-length: ${#MESSAGE}"
+  echo
+  echo -n "$MESSAGE"
+  exit 1
+}
+
+# Analyse de la requête entrante
 read STATUS_LINE
 >&2 echo "$(date -uR) - $STATUS_LINE"
 
 read -a REQUEST <<< "$STATUS_LINE"
 
-RESOURCE="$HOME/Public${REQUEST[1]}"
+RESOURCE=$(readlink -f "$HOME/Public${REQUEST[1]}")
+
+# Vérification des erreurs
 
 if [ ${REQUEST[0]} != "GET" ]
 then
-    MESSAGE="Method ${REQUEST[0]} not allowed"
-    echo "HTTP/1.1 405 $MESSAGE"
-    echo "Content-type: text/plain"
-    echo "Content-length: ${#MESSAGE}"
-    echo
-    echo -n $MESSAGE
-    exit 1
+  send_http_error 405 "Method ${REQUEST[0]} not allowed"
+fi
+
+if [[ "$RESOURCE" != $HOME\/Public\/* ]]
+then
+  send_http_error 403 "File access forbidden"
 fi
 
 if [ ! -f "$RESOURCE" ]
 then
-    MESSAGE="File ${REQUEST[1]} not found!"
-    echo "HTTP/1.1 404 NOT FOUND"
-    echo "Content-type: text/plain"
-    echo "Content-length: ${#MESSAGE}"
-    echo
-    echo -n $MESSAGE
-    exit 1
+  send_http_error 404 "File ${REQUEST[1]} not found!"
 fi
+
+# Envoi du fichier de réponse
 
 # FIX : la commande file ne retourne pas le bon MIME-TYPE pour les fichiers css
 if [[ "$RESOURCE" == *.css ]]
